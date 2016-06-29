5c33d41a32e996c4c836b81a24b2d7dec6a24879
Gareth Coles <gdude2002@gmail.com>
2016-06-29 13:06:32 +0100

    Path fixes, flake8 and the singleton metaclass.
    I only plan on using Singleton once in total.

--- Ultros/__main__.py
+++ Ultros/__main__.py
@@ -1,4 +1,9 @@
 # coding=utf-8
+import os
+import sys
+
+# We do this to ensure that our module-internal imports work
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 __author__ = 'Gareth Coles'
 
