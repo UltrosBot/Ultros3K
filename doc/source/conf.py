a28c9127671e17e62e05a6050f7abb24f17a0714
Gareth Coles <gdude2002@gmail.com>
2017-01-06 20:11:14 +0000

    Move source into src/ as per setup.py convention

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -20,7 +20,10 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../../'))
+print("===")
+print("Path: {}".format(os.getcwd()))
+print("===")
+sys.path.insert(0, os.path.abspath('../../src/'))
 
 # -- General configuration ------------------------------------------------
 
