64bce3266bd391df41080550ea159585d9acab6a
Gareth Coles <gdude2002@gmail.com>
2017-06-03 15:44:32 +0100

    [Ultros] Handle more shutdown signals

--- src/ultros/core/ultros.py
+++ src/ultros/core/ultros.py
@@ -4,6 +4,7 @@ import logging
 import signal
 
 import sys
+from typing import Optional
 
 from ultros.core.events import manager as event_manager
 from ultros.core.networks import manager as network_manager
@@ -26,12 +27,25 @@ __author__ = "Gareth Coles"
 
 
 class Ultros:
+    """
+    The Ultros class. Home for all system-wide components.
+    """
+
     event_manager = None
     network_manager = None
     plugin_manager = None
     storage_manager = None
 
-    def __init__(self, config_dir: str, data_dir: str, event_loop=None):
+    def __init__(self, config_dir: str, data_dir: str, event_loop: Optional[asyncio.BaseEventLoop]=None,
+                 handle_signals: bool=True):
+        """
+        :param config_dir: Directory containing configuration files
+        :param data_dir: Directory to contain data files
+        :param event_loop: Optionally, the event loop to work with. Omit this and the instance will create one itself,
+                           preferring `uvloop` if installed. If you don't want to use uvloop, pass in a loop yourself.
+        :param handle_signals: If you don't want SIGTERM, SIGINT and SIGBREAK handled automatically (eg, you have more
+                               than one Ultros instance), then set this to False.
+        """
         self.do_stop = False
 
         # TODO: Proper logging
@@ -60,31 +74,53 @@ class Ultros:
         self.plugin_manager = plugin_manager.PluginManager(self)
         self.network_manager = network_manager.NetworkManager(self)
 
-        signal.signal(signal.SIGINT, self._sigint)
+        if handle_signals:
+            signal.signal(signal.SIGINT, self._sigint)
+            signal.signal(signal.SIGTERM, self._sigterm)
+
+            if hasattr(signal, "SIGBREAK"):  # Windows only
+                signal.signal(signal.SIGBREAK, self._sigbreak)
+
+    def _sigterm(self, *_):
+        """
+        Handle SIGTERM and shut down gracefully.
+        """
+
+        self.log.debug("SIGTERM caught.")
+        asyncio.run_coroutine_threadsafe(self.shutdown(), self.event_loop)
 
     def _sigint(self, *_):
-        print("")
+        """
+        Handle CTRL+C or SIGINT and shut down gracefully.
+        """
+
+        print("")  # Or it'll print on the same line as the ^C
         self.log.debug("SIGINT caught.")
         asyncio.run_coroutine_threadsafe(self.shutdown(), self.event_loop)
 
+    def _sigbreak(self, *_):
+        """
+        Handle SIGBREAK (CTRL+BREAK or closing the CMD window) on Windows and shut down gracefully.
+
+        Note that we only get five seconds when this happens before the process is killed, so we should try
+        to keep this process fast.
+        """
+
+        # Windows-only - when someone tries to close their terminal.
+        # We only get 5 seconds to do this (which is hopefully enough), so keep an eye on shutdown times.
+
+        self.log.debug("SIGBREAK caught.")
+        asyncio.run_coroutine_threadsafe(self.shutdown(), self.event_loop)
+
     async def shutdown(self):
+        """
+        Gracefully shut down this Ultros instance.
+
+        In order, this unloads the network manager, plugin manager, event manager and storage manager,
+        and then stops the event loop if instructed to earlier (eg by calling `.run()`).
+        """
+
         self.log.info("Shutting down...")
-
-        if self.storage_manager:
-            try:
-                self.storage_manager.shutdown()
-            except Exception as e:
-                print(e)  # TODO: Logging
-
-            self.storage_manager = None
-
-        if self.event_manager:
-            try:
-                self.event_manager.shutdown()
-            except Exception as e:
-                print(e)  # TODO: Logging
-
-            self.event_manager = None
 
         if self.network_manager:
             try:
@@ -102,14 +138,46 @@ class Ultros:
 
             self.plugin_manager = None
 
+        if self.event_manager:
+            try:
+                self.event_manager.shutdown()
+            except Exception as e:
+                print(e)  # TODO: Logging
+
+            self.event_manager = None
+
+        if self.storage_manager:
+            try:
+                self.storage_manager.shutdown()
+            except Exception as e:
+                print(e)  # TODO: Logging
+
+            self.storage_manager = None
+
         if self.do_stop:
             self.event_loop.stop()
             self.event_loop.close()
 
     def setup(self):
+        """
+        Set up this Ultros instance.
+
+        This will parse configs, load plugins, and get networks connected. The managers however have already been
+        instantiated.
+        """
+
         # TODO
         pass
 
     def run(self):
+        """
+        Convenience function for running a single instance.
+
+        This will instruct the current instance to stop its event loop on shutdown, and will make that loop
+        run forever, blocking this thread with it.
+
+        If you don't want this, you can handle the event loop management yourself instead.
+        """
+
         self.do_stop = True
         self.event_loop.run_forever()
