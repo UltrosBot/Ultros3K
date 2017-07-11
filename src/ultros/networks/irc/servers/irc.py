f4f1286e4c0b18decc2b673771827dbc1ff0098e
Gareth Coles <gdude2002@gmail.com>
2017-07-11 09:33:08 +0000

    [IRC] Capabilities and wait for RPL_WELCOME before join

--- src/ultros/networks/irc/servers/irc.py
+++ src/ultros/networks/irc/servers/irc.py
@@ -9,6 +9,11 @@ __author__ = "Gareth Coles"
 class IRCServer(BaseServer):
     _connector = None
 
+    def __init__(self, name: str, network: "base_network.BaseNetwork"):
+        super().__init__(name, network)
+
+        self.server_capabilities = []
+
     @property
     def connector(self):
         if self._connector:
@@ -17,19 +22,34 @@ class IRCServer(BaseServer):
 
     async def connector_connected(self, connector):
         self._connector = ref(connector)
-        connector.transport.write(b"NICK Test\r\n")
-        connector.transport.write(b"USER test 0 * :Ultros\r\n")
+        self.connector.transport.write(b"CAP LS 302\r\n")
+        self.connector.transport.write(b"NICK Testros\r\n")
+        self.connector.transport.write(b"USER test 0 * :Ultros 3K\r\n")
 
     async def connector_disconnected(self, connector, exc):
         pass
 
     async def irc_UNHANDLED(self, tags, prefix, command, params):
         self.logger.debug(
-            "Line: tags={} / prefix={} / command={} / params={}".format(
+            "Unhandled line: tags={} / prefix={} / command={} / params={}".format(
                 repr(tags), repr(prefix), repr(command), repr(params)
             )
         )
 
     async def irc_PING(self, tags, prefix, command, params):
         self.connector.transport.write(b"PONG :" + params[0].encode("UTF-8") + b"\r\n")
-        self.connector.transport.write(b"JOIN #ultros\r\n")
+
+    async def irc_CAP(self, tags, prefix, command, params):
+        if params[0] == "*" and params[1] == "LS":
+            if params[2] == "*":
+                for cap in params[3].split(" "):
+                    self.server_capabilities.append(cap)
+            else:
+                for cap in params[2].split(" "):
+                    self.server_capabilities.append(cap)
+
+                self.logger.debug("Capabilities: {}".format(", ".join(self.server_capabilities)))
+                self.connector.transport.write(b"CAP END\r\n")
+
+    async def irc_001(self, tags, prefix, command, params):
+        self.connector.transport.write(b"JOIN #ultros-test\r\n")
